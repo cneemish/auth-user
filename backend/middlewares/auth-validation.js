1bbc2e62fab5406777f0ec1f41a2384e4e129a7e
cneemish <cneemish@gmail.com>
2025-03-11 19:33:23 +0530

    Changing models based on requirments and adding controller logic

--- backend/middlewares/auth-validation.js
+++ backend/middlewares/auth-validation.js
@@ -5,7 +5,12 @@ const signupValidation = (req, res, next) =>{
         firstName: Joi.string().min(3).max(100).required(),
         lastName: Joi.string().min(3).max(100).required(),
         email: Joi.string().email().required(),
-        password: Joi.string().min(8).max(16).required()
+        password: Joi.string()
+        .min(8)
+        .max(16)
+        .required()
+        .pattern(new RegExp(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/))
+        .message('Password must contain at least one uppercase letter, one number, and one special character (@$!%*?&), and be 8-16 characters long')
     })
     const {error} = schema.validate (req.body); // to validate the request body object 
     if (error){
--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -19,11 +19,11 @@ const userSchema = new schema({
         type: String,
         required: true,
         min: [8, 'Must be at least 6, got {VALUE}'],
-        max: [16, 'Exceed max characters of 16, got {VALUE}'],
-        match: [
+        max: [16, 'Exceed max characters of 16, got {VALUE}']
+        /* match: [
             /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/,
             'Password must contain at least one uppercase letter, one number, and one special character (@$!%*?&), and be 8-16 characters long'
-        ]  
+        ]   */
     
      }
 })
