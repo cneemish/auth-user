f9aa3c781038069c23c3168112b9ee2b9d4d116b
cneemish <cneemish@gmail.com>
2025-03-13 17:51:47 +0530

    Adding validation to the get call and displaying user data

--- backend/middlewares/profile-auth.js
+++ backend/middlewares/profile-auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
+const userModel = require('../models/user-model');
 
-const authProfile =  (req, res, next ) =>{
+/* const authProfile =  (req, res, next ) =>{
     const auth = req.headers['authtoken'];
     if (!auth){
         return res.status(403)
@@ -17,5 +18,32 @@ const authProfile =  (req, res, next ) =>{
             .json({ message: "Invalid Token or token expried"});
         }
 }
+ */
 
+const authProfile = async(req, res, next ) => {
+    try{
+        const auth = req.headers['authtoken'];
+        if (!auth){
+            return res.status(403)
+            .json({
+                message: "Unauthorized Access, Please pass auth token"
+            });
+            }
+
+            const decoded = jwt.verify(auth, process.env.JWT_SECRET);
+            req.user = await userModel.findById(decoded._id).select('-password');
+
+            if (!req.user){
+                return res.status(404)
+                .json({
+                    message: "User not found!"
+                });
+            }
+            next();
+
+    }catch(err){
+        return res.status(401)
+            .json({ message: "Invalid Token or token expried"});
+    }
+}
 module.exports = authProfile;
--- backend/routes/profile-router.js
+++ backend/routes/profile-router.js
@@ -4,9 +4,10 @@ const authProfile = require('../middlewares/profile-auth');
 
 router.get('/', authProfile, (req, res) =>{
     res.status(200).json([
-        console.log(req.user),
+        
         {
-            name: "test"
+            message: "User Profile fetched successfully",
+            user: req.user // This will display user details 
         }
     ])
 });
